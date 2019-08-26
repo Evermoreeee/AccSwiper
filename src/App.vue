83cb6cfae47fa94c7f73c7cfda99069e63a368cb
zx <acczhuang@163.com>
2019-08-26 18:22:16 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,185 @@
+<template>
+  <div id="app">
+    <h3>轮播图</h3>
+    <div id="swiper">
+        <div class="swiper-item">
+            <img src="./images/1.jpg" />
+        </div>
+        <div class="swiper-item">
+            <img src="./images/2.jpg" />
+        </div>
+        <div class="swiper-item">
+            <img src="./images/3.jpg" />
+        </div>
+    </div>
+
+    <h3>分类组</h3>
+    <div class="swiper-menus">
+        <div class="swiper-item">
+            <div class="menu">
+                <img src="./images/menu1.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu2.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu3.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu4.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu1.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu2.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu3.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu4.png" />
+                <p>分类</p>
+            </div>
+
+        </div>
+        <div class="swiper-item">
+            <div class="menu">
+                <img src="./images/menu1.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu2.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu3.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu4.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu1.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu2.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu3.png" />
+                <p>分类</p>
+            </div>
+            <div class="menu">
+                <img src="./images/menu4.png" />
+                <p>分类</p>
+            </div>
+
+        </div>
+    </div>
+
+    <h3>滑动tab切换</h3>
+    <div class="tabs">
+        <div class="tabs-item active">菜品</div>
+        <div class="tabs-item">评价</div>
+        <div class="tabs-item">商家</div>
+    </div>
+    <div class="swiper-tab">
+        <div class="swiper-item" style="background: rgb(111, 194, 3);">这里是菜品列表</div>
+        <div class="swiper-item" style="background: rgb(3, 147, 243);">这里是用户评价</div>
+        <div class="swiper-item" style="background: rgb(161, 3, 140);">这里是商家信息</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import AccSwiper from './swiper'
+export default {
+  name: 'app',
+  data () {
+    return {
+     
+    }
+  },
+  mounted(){
+    this.AccSwiperInit()
+  },
+  methods:{
+    AccSwiperInit(){
+      new AccSwiper({
+            swiper: '#swiper',      
+            item: '.swiper-item',
+      });
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+html {
+    font-size: 14px;
+    font-weight: 500;
+}
+body {
+    background: #f7f7f7;
+}
+* {
+    margin: 0;
+    padding: 0;
+}
+h3 {
+    text-align: center;
+    padding: 20px 0;
+}
+#swiper .swiper-item img{
+    width: 100%;
+}
+
+/* 分类菜单 */
+.swiper-menus .swiper-item {
+    display: flex;
+    flex-wrap: wrap;
+}
+.swiper-menus .swiper-item .menu {
+    width: 15%;
+    text-align: center;
+    margin: 10px 5%;
+}
+.swiper-menus .swiper-item .menu img {
+    width: 100%;
+}
+
+
+/* tab相关 */
+.tabs {
+    display: flex;
+    justify-content: center;
+}
+.tabs .tabs-item {
+    padding: 15px 0;
+    text-align: center;
+    width: 100%;
+}
+.tabs .tabs-item.active {
+    background: #fff;
+}
+.swiper-tab .swiper-item {
+    color: #fff;
+    font-size: 30px;
+    text-align: center;
+    padding: 100px 0;
+}
+
+
+</style>
---
+++ src/swiper.js
@@ -0,0 +1,220 @@
+
+export default class AccSwiper {
+    constructor(options = {}) {
+        this.options = Object.assign({
+            swiper: '#swiper',
+            item: '.swiper-name',
+            autoplay: false,
+            duration: 3000
+        }, options);
+        
+        // 所有的Element对象和集合
+        this.elements = {
+            swiper: document.querySelector(this.options.swiper),
+            items: null,
+            container: null,
+        };
+
+        // 一些状态
+        this.states = {
+            index: 0,           // 当前滑块的下标，从0开始
+            left: 0,            // container的一个left位置
+            touch: 0,           // 触摸状态 0：未触摸 1：手指触摸/鼠标按下
+            autoplay: this.options.autoplay,
+            touchTrack: {
+                start: null,    // 手指触摸/鼠标按下时的位置
+                old: null,      // 手指/鼠标上一次的位置
+            },
+        };
+
+        // 检测是在pc端还是移动端
+        this.isTouch = 'ontouchstart' in window; 
+
+        if (!this.elements.swiper) {
+            throw `${this.options.swiper} not found`;
+        }
+        
+        this.wrapContainer();
+        this.initStyle();
+        this.touchEvent();
+
+        if (this.options.autoplay) {
+            this.autoplay();
+            this.elements.swiper.addEventListener('mouseover', () => {
+                this.states.autoplay = false;
+            });
+            this.elements.swiper.addEventListener('mouseout', () => {
+                this.states.autoplay = true;
+            });
+        }
+
+        //resetTouchStatus
+        this.direction = '';
+        this.deltaX = 0;
+        this.deltaY = 0;
+        this.offsetX = 0;
+        this.offsetY = 0;
+    }
+    
+    /**
+     * @private 把滑块外层再包裹一个div
+     * 这个div负责整个滑块的移动
+     */
+    wrapContainer() {
+        const html = this.elements.swiper.innerHTML;
+        this.elements.swiper.innerHTML = '';
+        this.elements.container = document.createElement('div');
+        this.elements.container.innerHTML = html;
+        this.elements.swiper.append(this.elements.container);
+        this.elements.items = this.elements.swiper.querySelectorAll(this.options.item);
+    }
+    /**
+     * @private 初始化样式
+     */
+    initStyle() {
+        this.elements.swiper.setAttribute('style', `
+            position: relative; 
+            overflow: hidden;
+        `);
+        this.elements.container.setAttribute('style', `
+            display: flex;
+            width: 10000%;
+            position: relative;
+            left: 0;
+        `);
+        this.elements.items.forEach(item => {
+            item.style.cssText += `width: 1%;`;
+        });
+    }
+
+    /**
+     * description 监听触摸事件
+     */
+    touchEvent() {
+        if (this.isTouch) {
+            this.elements.container.addEventListener('touchstart', () => this.touchStart(event));
+            this.elements.container.addEventListener('touchmove', () => this.touchMove(event));
+            this.elements.container.addEventListener('touchend', () => this.touchEnd(event));
+        }
+        else {
+            this.elements.container.addEventListener('mousedown', () => this.touchStart(event));
+            this.elements.container.addEventListener('mousemove', () => this.touchMove(event));
+            this.elements.container.addEventListener('mouseup', () => this.touchEnd(event));
+        }
+    }
+
+    touchStart(event) {
+
+        this.resetTouchStatus()
+        // 阻止浏览器默认的拖拽行为
+        // // event.preventDefault();
+
+        this.states.touch = 1;
+        this.states.autoplay = false;
+        this.states.touchTrack.start = this.states.touchTrack.old = event.touches ? event.touches[0] : event;
+        
+        this.startX = event.touches[0].clientX;
+        this.startY = event.touches[0].clientY;
+    
+    }
+   
+    getDirection(x, y) {
+        const MIN_DISTANCE = 10
+        if (x > y && x > MIN_DISTANCE) {
+          return 'horizontal';
+        }
+      
+        if (y > x && y > MIN_DISTANCE) {
+          return 'vertical';
+        }
+      
+        return '';
+    }
+    _touchMove(event) {
+        var touch = event.touches[0];
+        this.deltaX = touch.clientX - this.startX;
+        this.deltaY = touch.clientY - this.startY;
+        this.offsetX = Math.abs(this.deltaX);
+        this.offsetY = Math.abs(this.deltaY);
+
+        this.direction = this.direction || this.getDirection(this.offsetX, this.offsetY);
+    }
+    touchMove(event) {
+        let _event = event
+        // 必须是手指/鼠标按下了才允许移动
+        if (this.states.touch != 1) return;
+
+        this._touchMove(_event)
+        // console.log(this.direction)
+        if(this.direction === 'horizontal'){
+            // console.log('preventDefault')
+            event.preventDefault();
+        } else{
+            //垂直滚动
+            return;
+        }
+        // 触摸和鼠标事件event不一样，要区分开来。
+        event = event.touches ? event.touches[0] : event;
+        
+        // event.pageX表示当前手指/鼠标所移动的位置
+        // 而我们this.states.touchTrack.old表示手指/鼠标的上一个位置
+        // 所以可以通过比对来判断是向左滑动还是向右滑动
+        if (event.pageX < this.states.touchTrack.old.pageX) {
+            this.states.left -= this.states.touchTrack.old.pageX - event.pageX;
+        }
+        else {
+            this.states.left += event.pageX - this.states.touchTrack.old.pageX;
+        }
+        this.states.touchTrack.old = event;
+        this.elements.container.style.left = this.states.left + 'px';
+    }
+
+    touchEnd(event) {
+        // 移除触摸状态
+        this.states.touch = 0;
+        this.states.autoplay = true;
+        event = event.changedTouches ? event.changedTouches[0] : event;
+        if (event.pageX < this.states.touchTrack.start.pageX) {
+            this.states.index ++;
+        }
+        else {
+            this.states.index --;
+        }
+
+        // 防止滑块溢出
+        if (this.states.index < 0) {
+            this.states.index = 0;
+        }
+        else if (this.states.index > this.elements.items.length - 1) {
+            this.states.index = this.elements.items.length - 1;
+        }
+
+        this.change(this.states.index);
+    }
+    resetTouchStatus() {
+        this.direction = '';
+        this.deltaX = 0;
+        this.deltaY = 0;
+        this.offsetX = 0;
+        this.offsetY = 0;
+    }
+    change(index) {
+        // 当前滑块的index乘以滑块宽度的相反数即为container的left位置。
+        this.states.left =  - (this.elements.items[0].offsetWidth * index);
+        this.elements.container.style.left = this.states.left + 'px';
+
+        // 用transition属性实现一个左右移动动画效果
+        this.elements.container.style.cssText += `transition: left 0.2s `;
+        // 当动画结束后，去掉transition属性
+        this.elements.container.addEventListener('transitionend', () => {
+            this.elements.container.style.cssText = this.elements.container.style.cssText.replace('transition', '');
+        });
+        
+        this.states.index = index;
+        // 触发一个事件
+        if (this.options.change && typeof this.options.change == 'function') {
+            this.options.change.bind(this)(index);
+        }
+    }
+   
+};
